82f88327a5f26c286c0dde3172376faa8f350e63
John <johnmlucia@gmail.com>
2012-01-08 21:53:24 -0800

    initial setup with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bmw::Application.config.secret_token = '7107ecaf8c6641d9f8b325f3233f0b75da208de7f3413a93046c341e4e5a63d43f09c5192843ab176a584af533a80e210a4b469bc5defacf1e247f020f7caa01'
