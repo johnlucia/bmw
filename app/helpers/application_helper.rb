7a807fa663ba191aa5cfcb0408c997f48ba1d632
John <johnmlucia@gmail.com>
2012-04-15 20:33:09 -0700

    right column stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
     object = RefinerySetting.find_by_name(name)
     simple_format(object.value) if object
   end
+  
+  def right_column
+    (@page.images.empty? && @page.content_for(:side_body).blank?) ? false : true
+  end
+  
+  def side_images
+    images = @page.images
+    images.inject(""){|tags,img| tags + image_tag(img.url)}.html_safe
+  end
 end
