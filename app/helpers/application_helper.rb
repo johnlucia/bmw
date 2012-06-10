079cb28565933bf8ed74f8dd8cc20d443293f38d
John <johnmlucia@gmail.com>
2012-06-10 13:02:56 -0700

    get map up and running on contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def custom_value(name)
     object = RefinerySetting.find_by_name(name)
-    simple_format(object.value) if object
+    object.value if object
   end
   
   def right_column
@@ -11,5 +11,9 @@ module ApplicationHelper
   def side_images
     images = @page.images
     images.inject(""){|tags,img| tags + image_tag(img.url)}.html_safe
+  end
+  
+  def extra_content
+    render :partial => "contact_page" if @page.title.eql?("Contact")
   end
 end
